be0a9863a2a3b2f3ab51e0eb4152bb7f63d38d11
William Overton <willovertonuk@gmail.com>
2021-10-28 12:11:21 +0100

    pull the latest from main, and the use this as your index.js

--- examples/basic-routing/src/index.js
+++ examples/basic-routing/src/index.js
@@ -1,72 +1,55 @@
-import { graphqlHandler } from "./graphql.js";
-import { Router } from "./router/index.js";
-import { getGraphQLParameters, processRequest, renderGraphiQL, sendResult, shouldRenderGraphiQL } from "graphql-helix";
+import { Router } from "../../../dist/index.js";
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  sendResult,
+  shouldRenderGraphiQL,
+} from "graphql-helix";
 import { schema } from "./schema";
 
 const router = new Router();
 
-router.use((req, res) => {
-  res.headers.set("x-powered-by", "FlightPath")
-})
+router.route("*", "/graphql", async (req, res) => {
+  let body = {};
 
-// router.use(graphqlHandler);
+  try {
+    const raw = await req.text();
+    body = JSON.parse(raw);
+  } catch (e) {}
 
-router.route("GET", "/", (req, res) => {
-  return res.send("Home");
-});
-
-router.get("/puppies", async (req, res) => {
-  res.headers.set("x-testing", "It works!");
-  return res.send("You're at the puppy page!");
-});
-
-router.get("/greeting/:name", async (req, res) => {
-  return res.send(`Hello ${req.params.name}!`);
-});
-
-router.get("/assets/*", async (req, res) => {
-  return res.send("This is where the assets would be!");
-});
-
-router.get("/graphql", async (req, res) => {
   const request = {
-    body: req.body,
-    headers: req.headers,
+    body: body,
+    headers: Object.fromEntries(req.headers.entries()),
     method: req.method,
     query: req.query,
   };
 
-  console.log("shouldRenderGraphiQL");
-  return res.send(renderGraphiQL());
+  if (shouldRenderGraphiQL(request)) {
+    res.send(renderGraphiQL());
+  } else {
+    // Extract the Graphql parameters from the request
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    // Validate and execute the query
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema,
+    });
+
+    sendResult(result, res);
+  }
 });
 
-router.post("/graphql", async (req, res) => {
-  const request = {
-    body: req.body,
-    headers: req.headers,
-    method: req.method,
-    query: req.query,
-  };
-  const { operationName, query, variables } = getGraphQLParameters(request);
-
-  const result = await processRequest({
-    operationName,
-    query,
-    variables,
-    request,
-    schema,
-  });
-  console.log("result");
-  console.log(JSON.stringify(result));
-
-  return sendResult(result, res);
-});
 /**
- * This must be last! Catch anything we dont handle and return a 404
+ * Run a route so the default handler doesnt throw a 404
  */
 router.route("*", "*", (req, res) => {
-  res.status = 404;
-  return res.send("Page not found!");
+  res.status = 404
+  res.send("Page Not Found")
 });
 
 router.listen();
